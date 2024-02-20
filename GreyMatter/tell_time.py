f41021e0b384d35ae43a0d3212e914eebd720155
Gison43 <37940882+Gison43@users.noreply.github.com>
2024-02-20 09:48:52 -0500

    Update tell_time.py
    Added  a "what day of the week is it" function

--- GreyMatter/tell_time.py
+++ GreyMatter/tell_time.py
@@ -1,9 +1,12 @@
-#this code is to retrieve the current date and time
+ #this code is to retrieve the current date and time
 
-from datetime import datetime as dt
+from datetime import datetime as dt, date
 
 from  SenseCells.tts_engine import tts
 
 def what_is_time():
   tts("The time is " + dt.strftime(dt.now(), '%H:%M:%S'))
-  
+
+def what_is_day():
+  current_date = dt.now().date()
+  tts "It is " + current_date.strftime("%A")
