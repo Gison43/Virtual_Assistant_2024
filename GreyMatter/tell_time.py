9745beab94a5b65cd50714591c08db13abddb51e
Gison43 <37940882+Gison43@users.noreply.github.com>
2024-02-20 09:09:56 -0500

    Update tell_time.py
    changed line 5 and updated it to 'SenseCells.tts_engine'

--- GreyMatter/tell_time.py
+++ GreyMatter/tell_time.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime as dt
 
-from  SenseCells.tts import tts
+from  SenseCells.tts_engine import tts
 
 def what_is_time():
   tts("The time is " + dt.strftime(dt.now(), '%H:%M:%S'))
