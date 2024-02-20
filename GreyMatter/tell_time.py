efbdd6db04e18debef97955c974e5148a28baca8
Gison43 <37940882+Gison43@users.noreply.github.com>
2024-02-20 09:08:32 -0500

    Update tell_time.py
    Added code to the tell_time.py file.

--- GreyMatter/tell_time.py
+++ GreyMatter/tell_time.py
@@ -1,1 +1,9 @@
+#this code is to retrieve the current date and time
 
+from datetime import datetime as dt
+
+from  SenseCells.tts import tts
+
+def what_is_time():
+  tts("The time is " + dt.strftime(dt.now(), '%H:%M:%S'))
+  
