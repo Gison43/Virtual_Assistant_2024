2024868cb03446e8833cdb1d7dacf8be0252e12a
Gison43 <37940882+Gison43@users.noreply.github.com>
2024-02-20 10:50:15 -0500

    Update tell_time.py
    Added the what_is_date function

--- GreyMatter/tell_time.py
+++ GreyMatter/tell_time.py
@@ -8,5 +8,9 @@ def what_is_time():
   tts("The time is " + dt.strftime(dt.now(), '%H:%M:%S'))
 
 def what_is_day():
-  current_date = dt.now().date()
-  tts "It is " + current_date.strftime("%A")
+  current_day = dt.now().date()
+  tts("It is " + current_day.strftime('%A'))
+
+def what_is_date():
+  current_date = dt.now()date()
+  tts("It is " + current_date.strftime('%B %d %Y'))
