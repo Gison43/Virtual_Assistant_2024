53c500eb547a02ae60ae5c156b58673ae3f4cd52
Gison43 <37940882+Gison43@users.noreply.github.com>
2024-02-20 11:11:13 -0500

    Update tell_time.py
    Added a days_from_now function

--- GreyMatter/tell_time.py
+++ GreyMatter/tell_time.py
@@ -14,3 +14,14 @@ def what_is_day():
 def what_is_date():
   current_date = dt.now()date()
   tts("It is " + current_date.strftime('%B %d %Y'))
+
+def days_from_now(year, month, day):
+ #get the current date
+    current_date = dt.now().date()
+
+#create a date time object for the futgure date
+   future_date = dt(year, month, day).date()
+
+#calculate the difference in days between the furtre date and the curent date
+   difference = (future_date - current_date).days
+   tts("There are " + str(difference) + " days remaining")
