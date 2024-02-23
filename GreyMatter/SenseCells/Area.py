a92755eb7852c4e85ac1cefa89565b30ea3b8786
Gison43 <37940882+Gison43@users.noreply.github.com>
2024-02-23 11:51:12 -0500

    Update Area.py
    This will have the VA calculate and spit out the area of a square

--- GreyMatter/SenseCells/Area.py
+++ GreyMatter/SenseCells/Area.py
@@ -2,19 +2,39 @@ from GreyMather import tts_engine
 
 #This code has the VA calculate the area of a rectangle or volume of a cube
 
+#function to preprocess the input and extract numerical value
+def extract_numerical_value(input_text):
+   #split the input text by space
+  words = input_text.split()
+  #initialize the numerical value as none
+  numerical_value = None
+#Iterate over the words in the input
+  for word in words:  #attempt to convert to word to a float
+    try:
+      numerical_value = float(word)
+      break  #exit the loop if a numerical value is found
+    except ValueError:
+      continue  #continue to the next word if the conversion fails
+
+  return numerical_value
+
 #Ask the length of the rectangle
 tts('What is the length of the rectangle in centimeters')
 
 #convert the voice input to a variable inp1
 inp1 = voice_to_text()
-tts('You just said {inp1}.')
-#ask the width of the rectangle
+length = extract_numerical_value(inp1)
+tts(f"You just said {inp1}.")
 
+#ask the width of the rectangle
 tts('What is the width of the rectangle')
+
 #convert the voice input to a variable inp2
 inp2 = voice_to_text()
-tts('You just said {inp2}.')
+width = extract_numerical_value(inp2)
+tts(f"You just said {inp2}")
+
 #Calculate the area
-area = float(inp1)*float(inp2)
+area = length * width
 #speak the results
-tts('The area of the rectange is {area} centimeters squared')
+tts(f"The area of the rectange is {area} square centimeters")
