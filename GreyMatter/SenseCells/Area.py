184b69254508ffcbd065bd5c65d963fb736695ba
Gison43 <37940882+Gison43@users.noreply.github.com>
2024-02-23 11:36:15 -0500

    Update Area.py
    First attempt at getting it to calculate the area of a square

--- GreyMatter/SenseCells/Area.py
+++ GreyMatter/SenseCells/Area.py
@@ -1,1 +1,20 @@
+from GreyMather import tts_engine
 
+#This code has the VA calculate the area of a rectangle or volume of a cube
+
+#Ask the length of the rectangle
+tts('What is the length of the rectangle in centimeters')
+
+#convert the voice input to a variable inp1
+inp1 = voice_to_text()
+tts('You just said {inp1}.')
+#ask the width of the rectangle
+
+tts('What is the width of the rectangle')
+#convert the voice input to a variable inp2
+inp2 = voice_to_text()
+tts('You just said {inp2}.')
+#Calculate the area
+area = float(inp1)*float(inp2)
+#speak the results
+tts('The area of the rectange is {area} centimeters squared')
