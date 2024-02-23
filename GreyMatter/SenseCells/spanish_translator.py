cac0837496b4d7c653349ffef041b81afb284052
Gison43 <37940882+Gison43@users.noreply.github.com>
2024-02-23 13:31:49 -0500

    Update spanish_translator.py

--- GreyMatter/SenseCells/spanish_translator.py
+++ GreyMatter/SenseCells/spanish_translator.py
@@ -3,20 +3,74 @@
 from io import BytesIO
 
 from tts_engine import tts
+from translate import Translator
 from pydub import AudioSegment #need to download with sudo install pydub
 from pydub.playback import play
+
+#Specify the input and output languages
+translator = Translator(from_lang="en", to_lang="es")
 
 lang_abbre = {"english":"en",
           "spanish": "es",
           "french": "fr"}
 
-lang = tts("What language do you want to use? You can choose english, spanish or french.")
+tts("What language do you want to use? You can choose english, spanish or french.")
    if check_message(['spanish']):
+      translator = Translator(from_lang="en", to_lang="es")
       spanish_translator.spanish()
-          tts("Ok, we'll practice spanish.")
+      tts("Ok, we'll practice spanish.")
          #if the message is true then call the function
 
    elif check_message(['french']):
-      spanish_translator.french()
+          translator = Translator(from_lang="en", to_lang="fr")
+          spanish_translator.french()
           tts("Ok, we'll practice french.")
+
+tts("Say something in english")
+#capture the spoken english
+
+with sr.Microphone() as source:
+          speech.adjust_for_ambien_noise(source)
+          try:
+                    audio = speech.listen(source)
+                    my_input = speech.recognize_google(audio, language="en")
+                    print(f"You said {my_input}")
+          except sr.UnknownValueError:
+                    pass
+
+#do the actual translation
+translation = translaor.translate(my_input)
+print(translation)
+#convert text to speech in Spanish
+tts = gTTS(text=translation, lang='es')
+#create a temporary file
+voice = BytesIO()
+#save the voice output as an audio file
+tts.write_to_fp(voice)
+#play the audio file
+voice.seek(0)
+play(AudioSegment.from_mp3(voice))
+#Prompt you to say something in Spanish
+tts("Say something in spanish")
+#capture spoken spanish
+with sr.Microphone() as source:
+          speech.adjust_for_ambient_noise(source)
+          try:
+                    audio = speech.listen(source)
+                    my_input = speech.recognize_google(audio, language='es')
+                    print(f"You said {my_input}")
+          except sr.UnknownValueError:
+                    pass
+Translator = Translator(from_lang='es', to_lang='en')
+translation = translator.translate(my_input)
+print(translation)
+#convert text to speech in Spanish
+tts = gTTS(text=translation, lang='en')
+#create temp file
+voice = BytesIO()
+#save voice output as an audio file
+tts.write_to_fp(voice)
+#play audio file
+voice.seek(0)
+play(AudioSegment.from_mp3(voice))
 
