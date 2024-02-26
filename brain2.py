5bec5e7bc8f900913a88dd291d3d1f143f74f753
Gison43 <37940882+Gison43@users.noreply.github.com>
2024-02-26 08:03:41 -0500

    Update brain2.py

--- brain2.py
+++ brain2.py
@@ -19,29 +19,29 @@ def brain(name, speech_text):
       else:
          return False
 
-   case ['who'|'are'|'you']):
+   case ['who'|'are'|'you']:
        general_conversations.who_are_you()
        #if the message is true then call the function
 
    case ['tell'|'joke']:
       general_conversations.tell_me_a_joke()
 
-   case ['how'|'long'|'until'|'my'|'birthday']) or check_message(['how', 'many', 'days', 'until', 'my', 'birthday']):
+   case ['how'|'long'|'until'|'my'|'birthday'] or check_message(['how', 'many', 'days', 'until', 'my', 'birthday']):
       tell_time.when_birthday()
    
-   case ['what'| 'year'|'is it']) or check_message(['what', 'current', 'year']):
+   case ['what'| 'year'|'is it'] or check_message(['what', 'current', 'year']):
       tell_time.current_year()
 
    case ['how'| 'old'| 'am'| 'i']:
       tell_time.how_old()
 
-   case(['calculate', 'area', 'rectangle']):
+   case['calculate'|'area'| 'rectangle']:
       area()
 
-   case(['when','my'| 'birthday']):
+   case['when'|'my'| 'birthday']:
       general_conversations.when_birthday()
 
-   case(['want'|'practice'| 'language']):
+   case['want'|'practice'| 'language']:
       tts("What language would you like to practice? Spanish or French?")
       #listen for the user's response
       user_response = get_user_input()
@@ -56,43 +56,43 @@ def brain(name, speech_text):
       else:
          tts("Sorry, only spanish and french are currently supported.")
 
-   case(['when'| 'new years']):
+   case['when'| 'new years']:
       tell_time.new_years_eve()
 
-   case(['when'| 'created']):
+   case['when'| 'created']:
       general_conversations.when_were_you_created()
 
-   case(['when'| 'born']):
+   case['when'| 'born']:
       general_conversations.when_where_you_born()
 
-   case(['how'|'are'|'you'|'doing']):
+   case['how'|'are'|'you'|'doing']:
       general_conversations.how_are_you()
 
-   case(['i am'| 'doing'| 'well'| 'fine'| 'good']):
+   case['i am'| 'doing'| 'well'| 'fine'| 'good']:
       general_conversations.i_am_doing_well_thank_you()
 
-   case(['who'| 'am'| 'i']) or check_message(['what','is','my', 'name']) or check_message(['what\'s my name']):
+   case['who'| 'am'| 'i'] or check_message(['what','is','my', 'name']) or check_message(['what\'s my name']):
       general_conversations.who_am_i()
     
-   case['time']) or check_message(['what', 'time', 'is', 'it']):
+   case['time'] or check_message(['what', 'time', 'is', 'it']):
       tell_time.what_is_time()
 
-   case['what'| 'day'| 'number']):
+   case['what'| 'day'| 'number']:
       tell_time.day_number()
 
-   case['what'|'day'|'is'|'it']) or check_message(['what', 'day', 'of', 'the', 'week', 'is','it']):
+   case['what'|'day'|'is'|'it'] or check_message(['what', 'day', 'of', 'the', 'week', 'is','it']):
       tell_time.what_is_day()
 
-   case['what'|'month']):
+   case['what'|'month']:
       tell_time.what_month()
 
-   case['what'| 'is'| 'the'| 'date'| 'today']) or check_message(['current', 'date']):
+   case['what'| 'is'| 'the'| 'date'| 'today'] or check_message(['current', 'date']):
      tell_time.what_is_date()
 
-   case['how'| 'many'| 'days'| 'remaining'| 'until']):
+   case['how'| 'many'| 'days'| 'remaining'| 'until']:
       #find the index of the word "until"
       #note that the speaker must ask for the date in the format of MM:DD:YYYY, in that order
-      if all(word in words for word in ['how', 'many', 'days', 'remaining', 'until']):
+      if all(word in words for word in ['how', 'many', 'days', 'remaining', 'until']:
          until_index = words.index('until')
 
    #extract the date provided by the speaker
