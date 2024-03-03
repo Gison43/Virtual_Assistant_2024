8d488a93cef2cf3c1b568ae6e11a8fa0d228c171
Gison43 <37940882+Gison43@users.noreply.github.com>
2024-03-03 08:45:55 -0500

    Update weather.py

--- GreyMatter/weather.py
+++ GreyMatter/weather.py
@@ -1,23 +1,29 @@
-import requests
-import xml.etree.ElementTree as ET
+from GreyMatter.SenseCells.tts_engine import tts
 
-# URL of the XML data with filter included
-url = "https://geo.weather.gc.ca/geomet?service=WFS&version=1.1.0&request=GetFeature&typename=CURRENT_CONDITIONS&filter=<Filter><PropertyIsEqualTo><PropertyName>name</PropertyName><Literal>Ottawa%20%28Richmond%20-%20Metcalfe%29</Literal></PropertyIsEqualTo></Filter>"
+def weather():
+    import requests
+    import xml.etree.ElementTree as ET
 
-# Send a GET request to the URL
-response = requests.get(url)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the XML content
-    root = ET.fromstring(response.content)
+    # URL of the XML data with filter included
+    url = "https://geo.weather.gc.ca/geomet?service=WFS&version=1.1.0&request=GetFeature&typename=CURRENT_CONDITIONS&filter=<Filter><PropertyIsEqualTo><PropertyName>name</PropertyName><Literal>Ottawa%20%28Richmond%20-%20Metcalfe%29</Literal></PropertyIsEqualTo></Filter>"
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the XML content
+        root = ET.fromstring(response.content)
     
-    # Find and print weather condition for Ottawa
-    for member in root.findall('.//{http://www.opengis.net/gml}featureMember'):
-        condition_en = member.find('.//{urn:x-msc-smc:md:weather-meteo}cond_en').text
+        # Find and print weather condition for Ottawa
+        for member in root.findall('.//{http://www.opengis.net/gml}featureMember'):
+            condition_en = member.find('.//{urn:x-msc-smc:md:weather-meteo}cond_en').text
         
-        # Print weather condition
-        print("Weather Condition (English):", condition_en)
-        break  # Only print the first condition found
-else:
-    print("Failed to retrieve weather data.")
+            # create the weather_resulst string
+            weather_result = f"Weather condition in Ottawa today is {condition_en}"
+            return weather_result
+    else:
+        return("Failed to retrieve weather data.")
+    
+    tts(weather_result)
