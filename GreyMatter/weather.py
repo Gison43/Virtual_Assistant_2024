d805d0749d6bac97965417d5b1b15128652d7c05
Gison43 <phycom@live.com>
2024-03-03 09:34:03 -0500

    weather.py function now works

--- GreyMatter/weather.py
+++ GreyMatter/weather.py
@@ -18,7 +18,7 @@ def weather():
         # Find and print weather condition for Ottawa
         for member in root.findall('.//{http://www.opengis.net/gml}featureMember'):
             condition_en = member.find('.//{urn:x-msc-smc:md:weather-meteo}cond_en').text
-            temperature = member.fine('.//{urn:x-msc-smc:md:weather-meteo}temp').text
+            temperature = member.find('.//{urn:x-msc-smc:md:weather-meteo}temp').text
             pressure = member.find('.//{urn:x-msc-smc:md:weather-meteo}pres_en').text
             pressure_tendency = member.find('.//{urn:x-msc-smc:md:weather-meteo}prestnd_en').text
 
@@ -32,12 +32,11 @@ def weather():
             # create the weather_resulst string
 
             weather_result = f"Weather in Ottawa today: Condition is {condition_en}. Temperature is {temperature} degrees Celsius. Atmospheric pressure is {pressure} kilopascals and {pressure_tendency_str}."
+            tts(weather_result)
             return weather_result
     else:
         tts("Failed to retrieve weather data.")
         return("Failed to retrieve weather data.")
-
-    tts(weather_result)
 
 if __name__ == "__main__":
     weather()
