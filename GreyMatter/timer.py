db6ba79e4aa3825c556bc53b9733b76d57c9e72d
Gison43 <phycom@live.com>
2024-03-09 12:41:54 -0500

    edited timer.py file

--- GreyMatter/timer.py
+++ GreyMatter/timer.py
@@ -20,23 +20,33 @@ def stopwatch():
         start_time = datetime.datetime.now()
         tts("Let's go.")
         return start_time
-    
+
     def stop():
         """Stops the timer.  Returns the time elapsed"""
         stop_time = datetime.datetime.now()
         total_time = (stop_time - start_time)
         tts(f"The stopwatch has been stopped and the total time is" {total_time} "minutes.")
-    
+        return stop_time
+
     def now():
         """Returns the current time with a message"""
         current_time = (datetime.datetime.now())
         tts("The stopwatch is at" {current_time})
         return current_time
-    
+
     def elapsed():
         """Time elapsed since start was called"""
         time_elapsed = (datetime.datetime.now() - start_time)
-        tts("Time elapsed since the start is" {time_elapsed})
+        hours, remainder = divmod(time_elapsed.total_seconds(), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        time_string = ""
+        if hours > 0:
+            time_string += f"{int(hours)} {'hour' if hours == 1 else 'hours'}"
+        if minutes > 0:
+            time_string += f"{int(minutes)} {'minutes' if minutes == 1 else 'minutes'}"
+        if seconds > 0:
+            time_string += f"{int(seconds)} {'second' if seconds == 1 else 'seconds'}"
+        tts("Time elapsed since the start is" {time_string})
         return time_elapsed
     
     def split():
