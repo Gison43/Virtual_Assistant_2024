4c98e5ee2884f5f27164cf4ec5aae1fd0d3dc93a
Gison43 <phycom@live.com>
2024-03-24 17:43:31 -0400

    Made changes to the list.py file and brain.py file

--- GreyMatter/list.py
+++ GreyMatter/list.py
@@ -2,37 +2,24 @@
 #This is code for a grocery list or any kind of list
 
 import os
-from user_input import get_user_input
 from GreyMatter.SenseCells.tts_engine import tts
 
 class List:
     def __init(self):
         self.items = []
 
-    def create_list(self):
-        tts("Sure.  Let's create a new list.")
-        tts("What would you like to name your new list?")
-        list_name = get_user_input()
-        items = []
-        while True:
-           item = tts("What would you like to add to the list? Say done when finished.")
-           if user_input == 'done':
-              break
-           items.append(item)
-        self.save_list(list_name, items)
-
-    def save_list(self, list_name, items):
+    def create_list(self, list_name):
         file_name = f"{list_name}.txt"
         with open(file_name, 'w') as f:
-            for item in items:
+            for item in self.items:
                 f.write(f"{item}\n")
         tts(f"List '{list_name}' has been created and saved")
 
-    def add_item(self,item):
+    def add_item(self,item, list_name):
         self.items.append(item)
         print(f"{item} added to the list called {list_name}")
 
-    def remove_items(self, item):
+    def remove_items(self, item, list_name):
         if item in self.items:
             self.items.remove(item)
             print(f"{item} removed from the list called {list_name}")
@@ -44,11 +31,14 @@ class List:
         if os.path.exists(file_name):
             with open(file_name, 'r') as f:
                 items = f.readlines()
-            tts(f"Here are the items in list called '{list_name}':")
+            if items:
+                tts(f"Here are the items in list called '{list_name}':")
             for item in items:
                 tts(item.strip())
+            else:
+                tts(f"The list called '{list_name}' is empty")
         else:
-            tts(f"Sorry list '{list_name}' does not exist.")
+            tts(f"Sorry the list called '{list_name}' does not exist.")
 
     def view_list(self):
         if self.items:
