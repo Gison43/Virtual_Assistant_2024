86a9e1505962061ffab16673f5454881332c4519
Gison43 <37940882+Gison43@users.noreply.github.com>
2025-03-02 14:03:25 -0500

    Update notes.py
    Added dictation

--- GreyMatter/notes.py
+++ GreyMatter/notes.py
@@ -9,7 +9,18 @@ def note_something(speech_text):
   words_of_message.remove ('note')
   cleaned_message = ' '.join (words_of_message)
   
-  conn.execute ("INSERT INTO notes (notes, notes_date) VALUES (?, ? )", (cleaned_message, datetime.strftime(datetime.now(), '%d-%m%-Y')))
+  conn.execute ("INSERT INTO notes (notes, notes_date) VALUES (?, ? )", (cleaned_message, datetime.strftime(datetime.now(), '%d-%m-%Y')))
   conn.commit()
   conn.close()
   tts('your note has been saved.')
+
+def show_all_notes():
+  conn = sqlite3.connect('memory.db')
+  tts('Your notes are as follows:')
+  
+  cursor = conn.execute("SELECT notes FROM notes")
+
+for row in cursor:
+  tts(row[0])
+
+conn.close()
