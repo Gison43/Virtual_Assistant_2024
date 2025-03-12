42bddf1a7065f0144b76d07299c4b990d9211e28
Gison43 <37940882+Gison43@users.noreply.github.com>
2025-03-12 17:21:20 -0400

    Update brain.py

--- brain.py
+++ brain.py
@@ -172,6 +172,10 @@ def neural_network(name, speech_text, city_name, city_code, stopwatch_instance, 
        my_list.create_list(list_name) #call the create_list method and pass the list name to it
        tts(f"Ok. I've created a list called {list_name}")
        time.sleep(2) # add a small delay to prevent immediate re-listening
+      #force the VA to ignore any lingering input before listening again
+       print("Flushing unintended input to prevent misinterpretation...")
+       _ = get_user_input()  #capture and discard the next speed input
+      print("Input flushed.  Ready for the next command.")
 
    elif check_message(['add','to', 'list']):
        tts("What is the name of the list?")
