99a814a2cfffe386d4d9a3944e82f59c3acf6894
Gison43 <37940882+Gison43@users.noreply.github.com>
2025-03-12 17:08:22 -0400

    Update brain.py

--- brain.py
+++ brain.py
@@ -170,6 +170,7 @@ def neural_network(name, speech_text, city_name, city_code, stopwatch_instance, 
        list_name = get_user_input()
        my_list.create_list(list_name) #call the create_list method and pass the list name to it
        tts(f"Ok. I've created a list called {list_name}")
+       time.sleep(2) # add a small delay to prevent immediate re-listening
 
    elif check_message(['add','to', 'list']):
        tts("What is the name of the list?")
