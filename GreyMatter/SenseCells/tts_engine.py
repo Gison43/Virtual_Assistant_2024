832132bab9be8b7b6d756d1640a1aaacc2273969
Gison43 <phycom@live.com>
2026-02-07 14:40:47 -0500

    removed sox because it's slow

--- GreyMatter/SenseCells/tts_engine.py
+++ GreyMatter/SenseCells/tts_engine.py
@@ -12,15 +12,10 @@ def tts(message, lang=None):
 
    if sys.platform == 'darwin':
       tts_engine = ['say', message]
+
    elif sys.platform.startswith('linux'):
-        # This is the 'Force conversion' logic we just verified
-        # It converts espeak output to 16kHz Stereo for the ReSpeaker HAT
-        aplay_cmd = "sox -t wav - -r 16000 -c 2 -t wav - | aplay -D hw:2"
-        
-        if lang == 'es':
-            command = f'espeak -v es "{message}" --stdout | {aplay_cmd}'
-        else:
-            command = f'espeak "{message}" --stdout | {aplay_cmd}'
-        
-        # We use shell=True to allow the piping (|) between espeak, sox, and aplay
-        subprocess.Popen(command, shell=True)
+    # We use 'plughw:2' to handle resampling and '-c 2' to force stereo
+    # This removes the need for the slow 'sox' middleman
+    command = f'espeak "{message}" --stdout | aplay -D plughw:2 -c 2'
+
+    subprocess.Popen(command, shell=True)
