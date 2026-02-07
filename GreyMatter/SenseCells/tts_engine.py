5f7b0b78f6813fee34def0cbdbdd09ef96055b9a
Gison43 <37940882+Gison43@users.noreply.github.com>
2026-02-07 14:57:02 -0500

    Update tts_engine.py
    Added '-t wav' to line 19.

--- GreyMatter/SenseCells/tts_engine.py
+++ GreyMatter/SenseCells/tts_engine.py
@@ -16,6 +16,6 @@ def tts(message, lang=None):
    elif sys.platform.startswith('linux'):
     # We use 'plughw:2' to handle resampling and '-c 2' to force stereo
     # This removes the need for the slow 'sox' middleman
-    command = f'espeak "{message}" --stdout | aplay -D plughw:2 -c 2'
+    command = f'espeak "{message}" --stdout | aplay -D plughw:2 -c 2 -t wav'
 
     subprocess.Popen(command, shell=True)
