fd4ef90b18b7a0b1de14230c9ccd2753331121c3
Gison43 <phycom@live.com>
2026-02-08 15:30:38 -0500

    Fix: Resolved ReSpeaker hw_params error using dmix and 44100Hz stereo force

--- GreyMatter/SenseCells/tts_engine.py
+++ GreyMatter/SenseCells/tts_engine.py
@@ -16,6 +16,6 @@ def tts(message, lang=None):
    elif sys.platform.startswith('linux'):
     # We use 'plughw:2' to handle resampling and '-c 2' to force stereo
     # This removes the need for the slow 'sox' middleman
-    command = f'espeak "{message}" --stdout | aplay -D plughw:2,0 -c 2 -r 16000 -c 1'
+    command = f'espeak "{message}" --stdout | aplay -D plug:dmix  -r 44100 -f S16_LE'
 
     subprocess.Popen(command, shell=True)
