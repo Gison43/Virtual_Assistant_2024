d8201d23e5361f00bdaa526e9e85dcde6065b902
Gison43 <37940882+Gison43@users.noreply.github.com>
2026-03-01 11:52:50 -0500

    Update nlp_brain.py
    Added more functions to nlp_brain.py

--- nlp_brain.py
+++ nlp_brain.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[27]:
-
 
 from datetime  import datetime as dt, date
 import random
+
+from GreyMatter import tell_time, general_conversations, spanish_translator, weather, define_subject, timer, sleep, play_music, area, notes
+from GreyMatter.list import List
+from GreyMatter.spanish_translator import language_selection
+from user_input import get_user_input
+from ear_test import check_ncf_email
 from GreyMatter.SenseCells.tts_engine import tts
 
+my_list = List()
 
 today = dt.now().date()
 now = dt.now()
@@ -17,13 +20,20 @@ def neural_network(name, text, city_name, city_code, stopwatch_instance, music_p
 
     knowledge_base = {
 
-         "who are you": random.choice (["I am computer, your dedicated and intelligent assistant.", "computer, God dammit did I not tell you before", "you keeping methat, I am computer."]),
-         "tell me a joke": random.choice([ "what do you call a magical dog? A labracadabrador.", "What do you call a pony with a cough? A little horse", "Why was six scared of seven? Because seven ate nine.", "What do you call a woman with one leg? Eileen", "what did the pirate say when he turned 80? Aye matey."]),
-         "how long until my birthday": "Your birthday is July 15.  You were born in 1978.",
-         "when were you created": "I was created on February 12 2024",
-         "how are you": "I am doing well, and you?",
-         "what is your name": "I dont have a name yet",
-         "what are you": "I am a fully functional, artificially intelligent, state of the art virtual assistant, and I am all yours, baby."
+        "who are you": random.choice (["I am computer, your dedicated and intelligent assistant.", "computer, God dammit did I not tell you before", "you keeping methat, I am computer."]),
+        "tell me a joke": random.choice([ "what do you call a magical dog? A labracadabrador.", "What do you call a pony with a cough? A little horse", "Why was six scared of seven? Because seven ate nine.", "What do you call a woman with one leg? Eileen", "what did the pirate say when he turned 80? Aye matey."]),
+        "when is my birthday": "Your birthday is July 15.  You were born in 1978.",
+        "when were you created": "I was created on February 12 2024",
+        "how are you": "I am doing well, and you?",
+        "what is your name": "I dont have a name yet",
+        "what are you": "I am a fully functional, artificially intelligent, state of the art virtual assistant, and I am all yours, baby."
+        "how old am i": tell_time.how_old(),
+        "calculate the area of a rectangle":area.calculate_rectangle_area(),
+        "when is new years":tell_time.new_years_eve(),
+        "how is the weather":weather.weather(),
+        "what is the weather like today":weather.weather(),
+        "when were you born":"I was created on February 12 2024",
+        
     }
 
     for key, response in knowledge_base.items():
