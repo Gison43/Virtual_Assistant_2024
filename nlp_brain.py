74f5f8a29de4fc1cebd9656d2b90b5cde705e3fd
Gison43 <37940882+Gison43@users.noreply.github.com>
2026-03-01 12:09:06 -0500

    Update nlp_brain.py

--- nlp_brain.py
+++ nlp_brain.py
@@ -20,6 +20,7 @@ def neural_network(name, text, city_name, city_code, stopwatch_instance, music_p
 
     knowledge_base = {
 
+        "who am i": f"You are {name}, my creator.",
         "who are you": random.choice (["I am computer, your dedicated and intelligent assistant.", "computer, God dammit did I not tell you before", "you keeping methat, I am computer."]),
         "tell me a joke": random.choice([ "what do you call a magical dog? A labracadabrador.", "What do you call a pony with a cough? A little horse", "Why was six scared of seven? Because seven ate nine.", "What do you call a woman with one leg? Eileen", "what did the pirate say when he turned 80? Aye matey."]),
         "when is my birthday": "Your birthday is July 15.  You were born in 1978.",
@@ -27,19 +28,20 @@ def neural_network(name, text, city_name, city_code, stopwatch_instance, music_p
         "how are you": "I am doing well, and you?",
         "what is your name": "I dont have a name yet",
         "what are you": "I am a fully functional, artificially intelligent, state of the art virtual assistant, and I am all yours, baby."
-        "how old am i": tell_time.how_old(),
-        "calculate the area of a rectangle":area.calculate_rectangle_area(),
-        "when is new years":tell_time.new_years_eve(),
-        "how is the weather":weather.weather(),
-        "what is the weather like today":weather.weather(),
+        "how old am i": tell_time.how_old,
+        "calculate the area of a rectangle":area.calculate_rectangle_area,
+        "when is new years":tell_time.new_years_eve,
+        "how is the weather":weather.weather,
+        "what is the weather like today":weather.weather,
         "when were you born":"I was created on February 12 2024",
         
     }
 
     for key, response in knowledge_base.items():
          if key in text:
-             tts(response)
-             return
+             if callable(response):
+                 return response()
+             return response
 
     if "time" in text:
         response = f"it's {dt.now().strftime('%I:%M %p')}"
